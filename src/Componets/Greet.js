9442ff9535f2b46cf3790f6a0bcfd22fc28fedb4
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-05-16 15:12:00 +0300

    updating files

---
+++ src/Componets/Greet.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+let SayHello=()=>{
+    return (
+        <div>hello world Now you are create your future</div>
+
+    );
+}
+
+
+export default SayHello ;
---
+++ src/Componets/Welcom.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+
+class Welcome extends Component{
+    render() {
+        return (
+            <h1>
+                <p>You can used a export withought defult method by addaing parnces</p>
+            </h1>
+
+
+        );
+    }
+
+}
+
+export default Welcome;
