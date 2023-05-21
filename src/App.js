e12623e7f339a822b90aa62431cc1adb8cfcb9d6
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-05-21 08:51:36 +0300

    dd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import SayHello from './Componets/Greet';
-import Welcome from './Componets/1-25/Welcom';
-import Hello from './Componets/Hello';
-import Massage from './Componets/1-25/Massage';
-import Counter from './Componets/1-25/Counter';
-import ChangeValue from './Componets/1-25/Setstae';
-import FunctionClick from './Componets/1-25/FunctionClick';
-import ClassClicked from './Componets/1-25/ClassClicked';
-import MassageBinding from './Componets/MassageComponent';
-import ParentComponent from './Componets/ParentComponent';
-import UserGreating from './Componets/1-25/UserGreating';
-import NameList from './Componets/1-25/NameList';
-import StyleCheet from './Componets/StyleCheet';
-import Inline from './Componets/Inline';
-import Form from './Componets/1-25/Form';
-import LifeCycleA from './Componets/lifeCycleA';
-import FragmentDemo from './Componets/1-25/FragmentDemo';
-import Table from './Componets/Table';
+
 
 const App=()=> {
   return (
