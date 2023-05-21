32d30f56cc459b4c4179f3c72e732410549198e8
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-05-21 09:11:09 +0300

    d

---
+++ src/Componets/B-(26-43)/RegulerComp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class RegulerComp extends Component {
+render() {
+    return (
+        <div>
+            Regular Components
+    
+        </div>
+    )
+}
+}
+
+export default RegulerComp
