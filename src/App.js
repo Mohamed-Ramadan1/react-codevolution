f86348a5a78aa74686230e4f91e2e079139c2ee6
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-05-21 09:08:14 +0300

    updating files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import  PureComp  from './Componets/B-(26-43)/PureComponent';
 
 
+// class App extends React.Component{
+//   render() {
+//     return (
+//       <div>
+//               <PureComp/>
+//       </div>
+
+//     )
+//   }
+// }
 const App=()=> {
   return (
     <div className="App">
+    <PureComp/>
 
     </div>
   );
---
+++ src/Componets/B-(26-43)/PureComponent.js
@@ -0,0 +1,15 @@
+import React, { PureComponent} from 'react';
+
+
+class PureComp extends PureComponent{
+    render() {
+        return (
+            <div>
+                PureComponent
+            </div>
+        )
+    }
+
+}
+
+export default PureComp;
