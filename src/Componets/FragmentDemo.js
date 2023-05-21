cbdc9886cbf281bd5b6b9f1ded193b9e4ab15469
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-05-21 08:13:05 +0300

    dd

--- src/Componets/FragmentDemo.js
+++ src/Componets/FragmentDemo.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const FragmentDemo = () => {
     return (
-        
+        <React.Fragment>
+                <h1>
+            Fragment FragmentDemo
+        </h1>
+            <p>This is fragment demo</p>
+        </React.Fragment>
+
     )
 }
 
