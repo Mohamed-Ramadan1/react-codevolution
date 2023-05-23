f76c24ab52ca9c99e4a875a51bf26c8b4dbbc51a
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-05-23 11:43:48 +0300

    updating some data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import Inputs from './Componets/B-(26-43)/Inputs';
-import GodFather from './Componets/B-(26-43)/GodFather';
-import FRParentInput from './Componets/B-(26-43)/FRParentInput';
+import PortalDemo from './Componets/B-(26-43)/PortalDemo';
 
 
 
@@ -10,6 +7,7 @@ import FRParentInput from './Componets/B-(26-43)/FRParentInput';
 const App=()=> {
   return (
     <div className="App">
+      <PortalDemo/>
 
     </div>
   );
---
+++ src/Componets/B-(26-43)/PortalDemo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PortalDemo = () => {
+    return (
+        <div>
+            <h1>Portal Demo</h1>
+        </div>
+    )
+}
+
+
+export default PortalDemo;
