2164c85157c950622a59d4dfdbc2c6966e2f9734
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-05-23 12:55:00 +0300

    hey

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import ErrorBoundery from './Componets/B-(26-43)/ErrorBoundery';
+import Hero from './Componets/B-(26-43)/Hero';
 import PortalDemo from './Componets/B-(26-43)/PortalDemo';
 
 
@@ -7,6 +9,14 @@ import PortalDemo from './Componets/B-(26-43)/PortalDemo';
 const App=()=> {
   return (
     <div className="App">
+      <ErrorBoundery>
+
+        <Hero heroName='poor' />
+
+      </ErrorBoundery>
+      
+      <Hero heroName='you can achive that '/>
+      <Hero heroName='You can save your self and your family more work meen fast leaving '/>
 
     </div>
   );
