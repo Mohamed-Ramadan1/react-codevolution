64b194d6744669e0d84c0a6ca6d4a0455ec20dfe
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-05 12:32:09 +0300

    s

--- src/ReactHooks/HookCounter.js
+++ src/ReactHooks/HookCounter.js
@@ -1,7 +1,7 @@
 import React, { useState} from 'react'
 
 function HookCounter() {
-    const [count, setCount] = useState(0);
+    const[ count, setCount] = useState(0);
 
     return (
             <div>
