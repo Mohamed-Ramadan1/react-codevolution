cbc6d3393654b77d3c1452dbc49b73182e119f04
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-09 01:39:40 +0300

    useEffect is easy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import HookCounterOne from './Componets/UseEffectHooks/HookCounterOne';
+import UseEffectOnce from './Componets/UseEffectHooks/UseEffectOnce';
 
 
 const App=()=> {
   return (
     <div className="App">
-      <HookCounterOne/>
-      
+      <UseEffectOnce/>
 
     </div>
   );
--- src/Componets/UseEffectHooks/UseEffectOnce.js
+++ src/Componets/UseEffectHooks/UseEffectOnce.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 function UseEffectOnce() {
+    const[x,setX]=useState(0)
+    const [Y, setY] = useState(0)
+    const getPostion = e => {
+    console.log('mouseEvent')
+    setX(e.clientX)
+    setY(e.clientY)
+}
+    useEffect(() => {
+        console.log("From Effect");
+        window.addEventListener('mousemove',getPostion)
+    },[])
     return (
         <div>
-        
+            Hooks x-{ x} Y-{Y }
         </div>
     )
 }
