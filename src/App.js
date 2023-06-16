bacbfe09c221e2e71134c5356117c2cf4ebfd575
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-16 17:25:24 +0300

    comp updaiting

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import './App.css';
 import ComponentC from './Componets/UseEffectHooks/ComponentC';
 
 
-
+  
 const App=()=> {
   return (
     <div className="App">
-      <ComponentC/>
 
     </div>
   );
