d094ac2a9ac675e95ca0cd19f130791f29b98b4d
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-16 23:21:43 +0300

    d

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import UseReducer from './Componets/Reducer/UseReducer';
 
 
   
 const App=()=> {
   return (
     <div className="App">
+      <UseReducer/ >
 
     </div>
   );
