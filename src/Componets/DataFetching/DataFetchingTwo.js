953bf5a9b49d5ea8de7a3951da2217f6fc954208
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-17 09:04:50 +0300

    sd

---
+++ src/Componets/DataFetching/DataFetchingTwo.js
@@ -0,0 +1,13 @@
+import React, { useReducer, useEffect } from 'react';
+import axios from 'axios';
+
+
+function DataFetchingTwo() {
+    return (
+        <div>
+        
+        </div>
+    )
+}
+
+export default DataFetchingTwo
