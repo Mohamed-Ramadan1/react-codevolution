316216c863776dc0bf130b441f8692ea076f3b30
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-17 09:04:56 +0300

    d

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import DataFetchingOne from './Componets/DataFetching/DataFetchingOne';
+import DataFetchingTwo from './Componets/DataFetching/DataFetchingTwo';
 
 
   
 const App=()=> {
   return (
     <div className="App">
-      <DataFetchingOne/>
+      {/* <DataFetchingOne/> */}
+      <DataFetchingTwo/>
     </div>
   );
 }
