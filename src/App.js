69f311c94f4427c7d16e3d7875ceb100b3b8e14b
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-17 08:38:49 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import ComponentCC from './Componets/ContextAPi/ComponentCC.JS';
 
 
   
 const App=()=> {
   return (
     <div className="App">
-      <ComponentCC/>
+
 
 
     </div>
