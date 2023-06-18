9af595f073624d0caf18faa62b68a1ab47531d2f
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-18 10:41:41 +0300

    updating app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
-import DataFetchingOne from './Componets/DataFetching/DataFetchingOne';
-import DataFetchingTwo from './Componets/DataFetching/DataFetchingTwo';
-import FocusInput from './Componets/UseRef/FocusInput';
-import UseCallback from './Componets/UseRef/UseCallback';
+
 
 
   
 const App=()=> {
   return (
     <div className="App">
-<FocusInput/>
     </div>
   );
 }
