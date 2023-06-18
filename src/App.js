8049917b9a2e47e832e0d88f01ea71a665c7250f
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-18 10:03:36 +0300

    Testing refs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import DataFetchingOne from './Componets/DataFetching/DataFetchingOne';
 import DataFetchingTwo from './Componets/DataFetching/DataFetchingTwo';
+import FocusInput from './Componets/UseRef/FocusInput';
 import UseCallback from './Componets/UseRef/UseCallback';
 
 
@@ -8,8 +9,7 @@ import UseCallback from './Componets/UseRef/UseCallback';
 const App=()=> {
   return (
     <div className="App">
-      <UseCallback/>
-
+<FocusInput/>
     </div>
   );
 }
