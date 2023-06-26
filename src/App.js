96f69fc80572641466556cf1ee3a28a162166901
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-06-26 22:13:50 +0300

    d

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import { Routes,Route } from "react-router-dom";
+import { Routes,Route, Link } from "react-router-dom";
 import About from "./Components/About";
 import Home from "./Components/Home";
+import { Fragment } from "react";
+import NavBar from "./Components/NavBar";
 
 
 const App=()=> {
   return (
+    <>
+      <NavBar/>.3
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/About" element={<About/>}></Route>
+        <Route path="/" element={<Home />}/>
+        <Route path="About" element={<About/>}/>
       </Routes>
 
 
 
-    </div>
+      </div>
+    </>
   );
 }
 
