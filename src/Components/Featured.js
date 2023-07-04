a20728a7be67e765e96173f88b63875aeae416fa
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-07-04 06:12:29 +0300

    KL

---
+++ src/Components/Featured.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Featured() {
+  return (
+    <div>
+      Featured data and informations
+    </div>
+  )
+}
+
+export default Featured;
--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import { Link, Outlet } from 'react-router-dom'
 function Product() {
     return (
+        <>
         <div>
-        <input type='search' placeholder='search'/>
-        </div>
+            <input type='search' placeholder='search'/>
+            </div>
+        <nav>
+            <Link to='featured'>Featured </Link>
+            <Link to='new'>New </Link>
+        </nav>
+            <Outlet />
+        </>
     )
 }
 
