c906426506cf9a6a52dc7271b59941bc4cbce638
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-07-11 08:38:05 +0300

    d

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
-import { Route,Routes } from "react-router-dom";
-import Home from "./Components/Home";
-import About from "./Components/About";
-import NavBar from "./Components/NavBar";
-import { Fragment } from "react";
-import OrderSummdry from "./Components/OrderSummry";
-import Nomatch from "./Components/Nomatch";
-import Product from "./Components/Product";
-import New from "./Components/New"; 
-import Featured from "./Components/Featured";
-import Users from "./Components/Users";
-import UserInformation from "./Components/UserInformation";
+
+
+
+
+
 const App=()=> {
   return (
     <div className="App">
-    
-      <Fragment>
-        <NavBar/>
-        <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path="about" element={ <About/>} />
-          <Route path="OrderSummry" element={<OrderSummdry />} />
-          
-          <Route   path="Product" element={<Product />} >
-            <Route path="new" element={ <New/>} />
-            <Route path="featured" element={ <Featured/>} />
-          </Route>
 
-          <Route path="/user" element={ <Users/>}>
-            <Route path=":userId" element={<UserInformation />} />
-          </Route>
-          
-          <Route path="*" element={ <Nomatch/>} />
-        </Routes>
-      </Fragment>
     </div>
-  
   );
 }
 
