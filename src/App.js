856119459787fcf49c315cc5e456e0323f475a22
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-07-11 10:52:05 +0300

    ssd1.21

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ import {
 
 import Home from './pages/Home';
 import About from './pages/About';
-import Faq from './pages/help/Faq'
-import Contact from './pages/help/Contact'
-
+import Faq from './pages/help/Faq';
+import Contact from './pages/help/Contact';
+import NoteFound from "./pages/NoteFound";
+import CareersLayout from "./layouts/CareersLayout";
+import Careers from './pages/careers/Careers'
 
 import RootLayOuts from "./layouts/RootLayOuts";
 import HelpLayOut from "./layouts/HelpLayOut";
-import NoteFound from "./pages/NoteFound";
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<RootLayOuts />}>
@@ -26,7 +27,16 @@ const router = createBrowserRouter(createRoutesFromElements(
       <Route path="faq" element={<Faq/>} />
       <Route path="contact" element={<Contact/>}/>
     </Route>
-    <Route path="*" element={<NoteFound/>} />
+
+      <Route path="careers" element={<CareersLayout />}>
+      <Route 
+        index 
+        element={<Careers />} 
+        loader={careersLoader} 
+      />
+    </Route>
+    <Route path="*" element={<NoteFound />} />
+    
   </Route> 
 ));
 
