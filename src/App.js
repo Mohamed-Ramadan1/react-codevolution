d3b614b2d5a4640adf112c7b674307e2662b52eb
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-07-11 09:40:49 +0300

    fdg

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 
+import Home from './pages/Home';
+import About from './pages/About';
 
+import RootLayOuts from "./layouts/RootLayOuts";
 
-
-
+const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={RootLayOuts}>
+        <Route index element={<Home/>}/>
+        <Route path="about" element={<About/>}/>
+    </Route> 
+));
+  
 const App=()=> {
   return (
-    <div className="App">
-
-    </div>
+      <RouterProvider router={router} />
   );
 }
 
